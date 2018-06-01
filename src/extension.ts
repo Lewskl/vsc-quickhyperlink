445561c9213158ecda85bc793f07f581a4350a1b
Leevi Aaltonen <leevia00@gmail.com>
2018-06-01 23:56:28 +0300

    Replaced template stuff with real stuff
    - no more "extension.sayHello"
    - added keybinds and command palette entry
    - changed display name and description

--- src/extension.ts
+++ src/extension.ts
@@ -9,12 +9,12 @@ export function activate(context: vscode.ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "quickhyperlink" is now active!');
+    console.log('quickhyperlink activated');
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    let disposable = vscode.commands.registerCommand('extension.makeLink', () => {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
